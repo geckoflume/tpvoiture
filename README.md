4eade89ad366f96549aacb4cca7987ad467c2101
Florian Mornet <fmornet@etudiant.univ-lr.fr>
2017-06-07 10:51:54 +0200

    Etape 2 : Début gestion des interruptions du bouton JOYSTICK_UP

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Microcontrôleur ARM Cortex M3 - jeu d'arcade "Circuit Voiture"
+# tpvoiture -  jeu d'arcade "Circuit Voiture"
 
-Projet final pour le module SI2, réalisé en binôme, sur [STM32F103ZG](http://www.st.com/content/ccc/resource/technical/document/reference_manual/59/b9/ba/7f/11/af/43/d5/CD00171190.pdf/files/CD00171190.pdf/jcr:content/translations/en.CD00171190.pdf "Reference") ([Keil MCBSTM32EXL](http://www.keil.com/arm/mcbstm32exl/)).
+Projet final pour le module SI2, réalisé en binôme, sur [STM32F103ZG](http://www.st.com/content/ccc/resource/technical/document/reference_manual/59/b9/ba/7f/11/af/43/d5/CD00171190.pdf/files/CD00171190.pdf/jcr:content/translations/en.CD00171190.pdf "Reference") ([Keil MCBSTM32EXL](http://www.keil.com/arm/mcbstm32exl/)).  
 Lien du sujet : [TPs_voiture_v1.1.pdf](TPs_voiture_v1.1.pdf)
 
 ## Étapes
--- src/main.c
+++ src/main.c
@@ -16,6 +16,9 @@
 #include "Voiture.h"
 
 #define UIF (1<<0)
+#define SETENA0 *(volatile unsigned long *)0xE000E100
+#define SETENA1 *(volatile unsigned long *)0xE000E104
+#define AFIO_EXTICR4 *(volatile unsigned long *)0x40010011
 
 void cfgTimer1(void){
 	 RCC->APB2ENR |= (1<<11);
@@ -24,6 +27,25 @@ void cfgTimer1(void){
 	 TIM1->DIER |= (1<<0);
 	 TIM1->CR1 |= 0x0001;
 	 *(volatile unsigned long *)0xE000E100 |= (1<<25);
+ }
+
+ void cfgGPIO(void){
+	 unsigned int temp;
+	 
+	 // Mise en place des registres AFIO pour la configuration d'interruptions externes
+	 RCC->APB2ENR |= (1<<0); //Enable AFIO Clock
+	 
+	 // Configuration de JOYSTICK_UP sur PG15
+	 RCC->APB2ENR |= (1<<8);
+	 temp = GPIOG->CRH & 0x0FFFFFFF;
+	 GPIOG->CRH = temp | 0x00000004;
+	 
+	 // Configuration des ISR sur EXT15 (JOYSTICK_UP)
+	 SETENA1 |= (1<<8);
+	 temp = AFIO_EXTICR4 &0xFFF0;
+	 AFIO_EXTICR4 = temp | 0x6FFF;
+	 EXTI->IMR |= (1<<15); //?
+	 EXTI->RTSR |= (1<<0);
  }
  
  // Traitement de l'interruption sur le timer TIM1
@@ -34,6 +56,19 @@ void cfgTimer1(void){
 		 voiture.y--;
 	 }
  }
+
+ // Traitement de l'interruption sur PG15
+  void EXTI15_10_IRQHandler(void){
+		if(EXTI->PR & (1<<15)){
+			EXTI->PR |=(1<<15);
+			/*
+			if(vitesse < 100){
+				vitesse +=20;
+			}
+			*/
+			voiture.x+=20;
+		}
+	}
  
  void clearVoiture(int x, int y){
 	GLCD_DrawBargraph(x, y, wbmpTuture, hbmpTuture, 0);
@@ -48,8 +83,8 @@ int main (void) {
 	voitureOld = voiture;
 	voitureOld.y--;
 	
-  	GLCD_Initialize();                          /* Initialize graphical LCD display   */
-  	GLCD_SetBackgroundColor(GLCD_COLOR_BLACK);
+  GLCD_Initialize();                          /* Initialize graphical LCD display */
+  GLCD_SetBackgroundColor(GLCD_COLOR_BLACK);
 	GLCD_SetForegroundColor(GLCD_COLOR_WHITE);
 	GLCD_ClearScreen(); 
 	
